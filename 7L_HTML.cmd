dae1738e9c77dd266cc1dc22dacf40b2f685bdb7
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-06 15:42:35 -1000

    Add files via upload

---
+++ 7L_HTML.cmd
@@ -0,0 +1,15 @@
+cd /
+:top
+cls
+mod con: lines=6 cols=30
+title "HTML_7L"
+@cecho off & cecho {04}
+@set "HTML=https://htmlpreview.github.io/?https://github.com/Kiaaina/HTML_7L/blob/master/Kia7L.html"
+
+start "%HTML%"
+goto 7L
+
+:7L
+cecho {05}Made_By_Kia7L{02}
+@timeout /t 5
+goto top
