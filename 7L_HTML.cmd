c1bc314a7096ee43cee6f30ad80ca7d34279bbb8
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-10-13 14:55:12 -1000

    Add files via upload

--- 7L_HTML.cmd
+++ 7L_HTML.cmd
@@ -1,16 +1,16 @@
 cd /
-:top
-mod con: lines=3 cols=20
 title "HTML_7L"
+color 74
+mode 50,2
 echo off
 cls
-cecho off & cecho {04}
+
 set "HTML=https://htmlpreview.github.io/?https://github.com/Kiaaina/HTML_7L/blob/main/7L_HTML.html"
-%HTML%
+start %HTML%
 goto 7L
 
 :7L
 cecho {05}Made_By_Kia7L{02}
-@timeout /t 5
+(timeout /t 5) > nul
 exit
 
